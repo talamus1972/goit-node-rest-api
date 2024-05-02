f12129ade55554cf38e767e8f40003769ce913b0
talamus1972 <talamus1972@gmail.com>
2024-05-02 20:26:17 +0300

    3

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,12 +4,12 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-})
-  .min(1)
-  .message("Body must have at least one field");
+});
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-});
+})
+  .min(1)
+  .message("Body must have at least one field");
