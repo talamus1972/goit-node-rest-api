02795fa0375206f9443834bd40365d752e15faa5
talamus1972 <talamus1972@gmail.com>
2024-05-06 16:22:43 +0300

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,48 @@
 import User from "../models/user.js";
 import HttpError from "../helpers/HttpError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const { SECRET_KEY } = process.env;
 
 export const register = async (req, res, next) => {
   try {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (user) {
       throw HttpError(409, "Email in use");
     }
+    const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
+
     res.status(201).json({ email: newUser.email });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    const payload = {
+      id: user._id,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "240h" });
+
+    res.json({ token });
   } catch (error) {
     next(error);
   }
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { validateBody } from "../middlewares/index.js";
 import { registerSchema, loginSchema } from "../schemas/usersSchemas.js";
-import { register } from "../controllers/auth.js";
+import { register, login } from "../controllers/auth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), register);
+authRouter.post("/login", validateBody(registerSchema), login);
 
 export default authRouter;
