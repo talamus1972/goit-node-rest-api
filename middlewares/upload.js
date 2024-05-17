9737145ca442f4fe2a643f211d0ae7699fba007e
talamus1972 <talamus1972@gmail.com>
2024-05-17 20:17:45 +0300

    2

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,20 +1,23 @@
-import multer from "multer"
-import path from "node:path"
-import crypto from "node:crypto"
+import multer from "multer";
+import path from "node:path";
 
-const multerCongif = multer.diskStorage({
+const multerConfig = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve("tmp")); 
+    cb(null, path.resolve("tmp"));
   },
-    filename: function (req, file, cb) {
-        const extname = path.extname(file.originalname)
-        const basename = path.basename(file.originalname, extname)
-        const suffix = crypto.randomUUID() 
-    cb(null, `${basename}-${suffix}${extname}`); 
+  filename: function (req, file, cb) {
+    const extname = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, extname);
+
+    if (!req.user || !req.user._id) {
+      return cb(new Error("User ID not found in request"));
+    }
+
+    const userId = req.user._id;
+    cb(null, `${basename}-${userId}${extname}`);
   }
 });
 
-
 export const upload = multer({
-    storage: multerCongif
-})
+  storage: multerConfig
+});
